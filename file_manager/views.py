fa32efe500ab24cdb008f3dd152d00071cbb82e9
Nicholas Studt <nicholas@nicholasstudt.com>
2009-01-31 04:43:01 +0000

    Updates.

--- file_manager/views.py
+++ file_manager/views.py
@@ -1,7 +1,44 @@
+import os
+import urllib
+
 from django import http, template
+from django.core.exceptions import ImproperlyConfigured
 from django.conf import settings
 from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import render_to_response
+
+def _get_document_root():
+    if not settings.DOCUMENT_ROOT:
+        raise ImproperlyConfigured, 'file_manager requires DOCUMENT_ROOT variable be defined in settings.py' 
+
+    return settings.DOCUMENT_ROOT
+
+def _clean_path(url):
+    """
+    Makes the path safe from ..
+    """
+    if not url:
+        return '' 
+
+    result = ''
+    path = os.path.normpath(urllib.unquote(url))
+    path = path.lstrip('/')
+    for part in path.split('/'):
+        if not part:
+            # Strip empty path components.
+            continue
+        drive, part = os.path.splitdrive(part)
+        head, part = os.path.split(part)
+        if part in (os.curdir, os.pardir):
+            # Strip '.' and '..' in path.
+            continue
+        result = os.path.join(result, part).replace('\\', '/')
+        
+    if result and path != result or not path:
+        result = ''
+    
+    return result
+    
 
 @staff_member_required
 def create(request, url=None):
@@ -11,23 +48,60 @@ def create(request, url=None):
 def copy(request, url=None):
     pass
 
-@staff_member_required
 def file_index(request, url=None):
     """
     Show list of files in a directory
     """
+  
+    perms = [ '---', '--x', '-w-', '-wx', 'r--', 'r-x', 'rw-', 'rwx' ]
+
+    clean_url = _clean_path(url)
 
     # Stuff the files in here.
     files = []
 
-    # 
+    full_path = os.path.join(_get_document_root(), clean_url)
+
+    listing = os.listdir(full_path)
+
+    for file in listing:
+        itemstat = os.stat(os.path.join(full_path, file))
+
+        item = {}
+        # name
+        item['filename'] = file
+
+        # size (in bytes ) for use with |filesizeformat
+        item['size'] = itemstat.st_size
+        
+        # type (direcory/file)
+        item['directory'] = os.path.isdir(file)
+
+        # ctime
+        item['ctime'] = itemstat.st_ctime
+        # mtime
+        item['mtime'] = itemstat.st_mtime
+
+        # permissions (numeric)
+        octs = "%04d" % int(oct(itemstat.st_mode & 0777))
+        dperms = '-'
+        if item['directory']:
+            dperms = 'd'
+
+        item['perms_numeric'] = octs
+        item['perms'] = "%s%s%s%s" % ( dperms, perms[int(octs[1])], perms[int(octs[2])], perms[int(octs[3])] )
+        
+        files.append(item)
+    
+    # Sort the files array based on something...
 
     return render_to_response("admin/file_manager/index.html", 
                               {
-                                'directory': url,
+                                'directory': clean_url,
                                 'listing': files,
                                },
                               context_instance=template.RequestContext(request))
+file_index = staff_member_required(file_index)
 
 @staff_member_required
 def mkdir(request, url=None):
