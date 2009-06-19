da732a60aed147d462f49c69884af0b9f1286496
Nicholas Studt <nicholas@nicholasstudt.com>
2009-06-19 15:29:49 +0000

    Updates.

--- file_manager/forms.py
+++ file_manager/forms.py
@@ -7,70 +7,39 @@ from django.utils.translation import ugettext as _
 
 from file_manager import utils
 
-class DirectoryPathField(forms.ChoiceField):
-    """ 
-        Copied directly from FilePathField, alter to show only directories
-        only, rather than files.
-
-        Also fixed the sorting.
-
-        showroot=True -- Adds root directory to the options.
-    """
-    def __init__(self, path, showroot=False, match=None, 
-                 recursive=False, required=True, widget=None, label=None, 
-                 initial=None, help_text=None, *args, **kwargs):
-        self.path, self.match, self.recursive = path, match, recursive
-        super(DirectoryPathField, self).__init__(choices=(), required=required,
-            widget=widget, label=label, initial=initial, help_text=help_text,
-            *args, **kwargs)
-        self.choices = []
-
-        # Make "/" valid"
-        if showroot:
-            d = self.path
-            d_short = d.replace(path, "", 1)
-            if not d_short:
-                d_short = '/'
-            self.choices.append((d, d_short))
-
-        if self.match is not None:
-            self.match_re = re.compile(self.match)
-        if recursive:
-            for root, dirs, files in os.walk(self.path):
-                for d in dirs:
-                    if self.match is None or self.match_re.search(f):
-                        d = os.path.join(root, d)
-                        d_short = d.replace(path, "", 1)
-                        self.choices.append((d, d_short))
-        else:
-            try:
-                for f in os.listdir(self.path):
-                    full_file = os.path.join(self.path, f)
-                    if os.path.isdir(full_file) and (self.match is None or self.match_re.search(f)):
-                        self.choices.append((full_file, full_file.replace(path, "", 1)))
-            except OSError:
-                pass
-        self.widget.choices = sorted(self.choices)
-
 class DirectoryForm(forms.Form):
 
-    document_root = utils.get_document_root()
-
-    parent = DirectoryPathField(path=document_root,recursive=True,showroot=True, help_text=_('Destination Directory'))
+    parent = forms.ChoiceField(help_text=_('Destination Directory'))
 
     def __init__(self, file, original, *args, **kwargs):
         self.file = file 
         self.original = original 
+        self.document_root = utils.get_document_root()
         super(DirectoryForm, self).__init__(*args, **kwargs)
+    
+        # Set the choices dynamicly.
+        self.fields['parent'].choices = self.make_choices()
 
-        # flip through this and remove everything that matches original
-        # self.fields['parent'].choices 
-        filtered = []
-        for choice in self.fields['parent'].choices:
-            if not choice[0].startswith(self.original): 
-                filtered.append(choice)
+    def make_choices(self):
+ 
+        choices = []
 
-        self.fields['parent'].choices = filtered
+        # Make "/" valid"
+        d = self.document_root
+        d_short = d.replace(self.document_root, "", 1)
+        if not d_short:
+            d_short = '/'
+        
+        choices.append((d, d_short))
+
+        for root, dirs, files in os.walk(self.document_root):
+            for d in dirs:
+                d = os.path.join(root, d)
+                if not d.startswith(self.original): 
+                    d_short = d.replace(self.document_root, "", 1)
+                    choices.append((d, d_short))
+
+        return sorted(choices)       
 
     def clean_parent(self):
         parent = self.cleaned_data['parent']
