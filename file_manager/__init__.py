10dd9d8b33584b0ff488bda8e10ba88d8c77d781
Nicholas Studt <nicholas@nicholasstudt.com>
2009-09-09 03:30:12 +0000

    Fixing version numbers so they make sense.

--- file_manager/__init__.py
+++ file_manager/__init__.py
@@ -1,7 +1,10 @@
-VERSION = (1, 0, 0, 'rc', 4)
+VERSION = (1, 0, 0, 'bugfix', 20090625)
+#          0  1  2   3    4
+# 1.0-YYYYMNDD
+# 1.0-YYYYMNDD
 
 def get_version():
-    version = '%s.%s.%s %s' % (VERSION[0], VERSION[1], VERSION[2], VERSION[3]) 
+    version = '%s.%s.%s' % (VERSION[0], VERSION[1], VERSION[2]) 
     
     if VERSION[3] != 'final':
         version = '%s %s' % (version, VERSION[4])
