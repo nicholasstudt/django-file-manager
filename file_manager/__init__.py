ab0340cf33675da884986763bd12ab8db8666051
Nicholas Studt <nicholas@nicholasstudt.com>
2009-10-22 03:06:37 +0000

    Fixing version numbers.

--- file_manager/__init__.py
+++ file_manager/__init__.py
@@ -1,13 +1,9 @@
-VERSION = (1, 0, 0, 'bugfix', 20090625)
-#          0  1  2   3    4
-# 1.0-YYYYMNDD
-# 1.0-YYYYMNDD
+VERSION = (1, 0, 3)
 
 def get_version():
-    version = '%s.%s.%s' % (VERSION[0], VERSION[1], VERSION[2]) 
+    version = '%s.%s' % (VERSION[0], VERSION[1]) 
     
-    if VERSION[3] != 'final':
-        version = '%s %s' % (version, VERSION[4])
+    if VERSION[2] > 0: # BUG Fix releases.
+        version = '%s.%s.%s' % (VERSION[0], VERSION[1], VERSION[2]) 
 
     return version
-
